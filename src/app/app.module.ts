1721ef1dc3e808a100070befe23c98e51a15362d
Singh <jimsingh@caswom1078728.ca.deloitte.com>
2018-10-11 13:04:39 -0400

    Add Angular Material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,11 @@ import {PostCreateComponent} from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html'
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
+    enteredValue = '';
     newPost = ''; //Property of class. Doesn't require const, var, let
 
     onAddPost(postInput: HTMLTextAreaElement){
         // alert('Post added');
-        
-        this.newPost = postInput.value;
+        this.newPost = this.enteredValue;
     }
 }
