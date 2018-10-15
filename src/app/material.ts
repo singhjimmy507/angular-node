593c331af18ea23851b710b35e50cb24722faba4
Singh <jimsingh@caswom1078728.ca.deloitte.com>
2018-10-15 16:22:13 -0400

    Basic form setup

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,11 +1,16 @@
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { NgModule } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatIconModule, MatInputModule, MatRadioModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatAutocompleteModule, MatIconModule, MatInputModule, MatRadioModule],
+  // tslint:disable-next-line:max-line-length
+  imports: [MatButtonModule, MatAutocompleteModule, MatIconModule, MatInputModule, MatRadioModule, MatCardModule, MatDatepickerModule],
+  // tslint:disable-next-line:max-line-length
+  exports: [MatButtonModule, MatAutocompleteModule, MatIconModule, MatInputModule, MatRadioModule, MatCardModule, MatDatepickerModule],
 })
 export class MaterialModule { }
